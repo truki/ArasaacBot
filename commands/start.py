7b8f24d7a75d22ec3673e5dd6c17b0e3733329bb
trukise <trukise@gmail.com>
2017-04-24 17:40:01 +0200

    Little modifications on start command (visual modifications)

--- commands/start.py
+++ commands/start.py
@@ -2,7 +2,14 @@ def start(bot, update):
     # Home message
     msg = "Hello {user_name}! I'm {bot_name}. \n"
     msg += "What would you like to do? \n"
-    msg += "/getPicColor + name - List color pictograms that contains name \n"
+    msg += "Pictograms: \n"
+    msg += "========================================\n"
+    msg += "/getPicsColor <text1,text2,...> - List color pictograms that contains textn words \n"
+    msg += "/getPicsBN <text1,text2,...> - List BN pictograms that contains textn words \n"
+    msg += "\n"
+
+    msg += "Other commands: \n"
+    msg += "========================================\n"
     msg += "/about - shows about me information"
 
     # Send the message
