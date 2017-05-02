bf549985ce1bd62b1808238ea20474037054d5df
Sergio Sánchez <trukise@gmail.com>
2017-05-02 23:47:55 +0200

    Change KeyboarButtons to InlineKeyBoardButtons inside ReplyKeyboardMarkup, on commands /pics and /translate

--- commands/translate.py
+++ commands/translate.py
@@ -9,12 +9,12 @@ def translate(bot, update, args):
     print("User: {}".format(str(update.message.from_user.id)))
 
     # Fist stage the user must to choose the language
-    keyboard = [[telegram.KeyboardButton("Español", callback_data='translate.language.ES'),
-                 telegram.KeyboardButton("English", callback_data='translate.language.EN'),
-                 telegram.KeyboardButton("French", callback_data='translate.language.FR')],
-                [telegram.KeyboardButton("Italian", callback_data='translate.language.IT'),
-                 telegram.KeyboardButton("Catalan", callback_data='translate.language.CA'),
-                 telegram.KeyboardButton("German", callback_data='translate.language.GE')]
+    keyboard = [[telegram.InlineKeyboardButton("Español", callback_data='translate.language.ES'),
+                 telegram.InlineKeyboardButton("English", callback_data='translate.language.EN'),
+                 telegram.InlineKeyboardButton("French", callback_data='translate.language.FR')],
+                [telegram.InlineKeyboardButton("Italian", callback_data='translate.language.IT'),
+                 telegram.InlineKeyboardButton("Catalan", callback_data='translate.language.CA'),
+                 telegram.InlineKeyboardButton("German", callback_data='translate.language.GE')]
                  ]
     bot.send_message(chat_id=update.message.chat_id,
                  text="wellcome, translate '"+str(args)+"?', choose the language:",
